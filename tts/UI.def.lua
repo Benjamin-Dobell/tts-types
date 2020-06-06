6faf7188e5975b31af638b3f05b587b78461cb39
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2020-06-07 04:16:14 +1000

    UI.getXml(), UI.getXmlTable() & UI.setXmlTable()

--- tts/UI.def.lua
+++ tts/UI.def.lua
@@ -5,8 +5,20 @@ local UI
 ---@field name string
 ---@field url string
 
+---@return string
+function UI.getXml() end
+
+---@return table<string, any>[]
+function UI.getXmlTable() end
+
 ---@overload fun(xml: string)
 ---@param xml string
 ---@param assets tts__UICustomAsset[]
 ---@return boolean
 function UI.setXml() end
+
+---@overload fun(xmlTable: table<string, any>[])
+---@param xml table<string, any>[]
+---@param assets tts__UICustomAsset[]
+---@return boolean
+function UI.setXmlTable() end
