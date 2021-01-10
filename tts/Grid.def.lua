c5f96835ba37fca967b6aadc40d81a0817d1dece
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2021-01-11 04:50:12 +1100

    BREAKING: Improved naming consistency for Grid

--- tts/Grid.def.lua
+++ tts/Grid.def.lua
@@ -1,13 +1,13 @@
----@alias tts__GridType 1 | 2 | 3
----@alias tts__GridSnapping 1 | 2 | 3 | 4
+---@alias tts__Grid_Type 1 | 2 | 3
+---@alias tts__Grid_Snapping 1 | 2 | 3 | 4
 
----@class tts__GlobalGrid
----@field type tts__GridType
+---@class tts__Grid
+---@field type tts__Grid_Type
 ---@field show_lines boolean
 ---@field color tts__Color @TODO: Luanalysis limitation at time of writing. Technically the "setter" ought to be typed tts__ColorShape
 ---@field opacity number
 ---@field thick_lines boolean
----@field snapping tts__GridSnapping
+---@field snapping tts__Grid_Snapping
 ---@field offsetX number
 ---@field offsetY number
 ---@field sizeX number
