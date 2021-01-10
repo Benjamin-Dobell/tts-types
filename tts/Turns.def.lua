99514750eb2350945b3fee68ee440a4f85698aae
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2021-01-11 05:14:47 +1100

    Turns definitions

--- tts/Turns.def.lua
+++ tts/Turns.def.lua
@@ -1,3 +1,19 @@
+-- Auto | Custom
+---@alias tts__TurnsType 1 | 2
+
 ---@class tts__Turns
----@field enable boolean
+---@field enable boolean @Whether the turn system is enabled.
+---@field type tts__TurnsType @The turn progression scheme being used. Automatic (built-in default) or a custom user provided order. 1 = Auto, 2 = Custom
+---@field order tts__PlayerHandColor[] @Turn order represented by an array of player colours.
+---@field reverse_order boolean @Whether turns should take place in the reverse order to that which has been specified.
+---@field skip_empty_hands boolean @Skips a player's turn if there are no object in the player's (primary) hand. WARNING: Does not presently work when using a custom turn ordering.
+---@field disable_interactations boolean @When enabled, players whose turn it is not will be unable to interact with objects.
+---@field pass_turns boolean @Whether the player whose turn it currently is permitted to choose to pass their turn to any other seated player.
+---@field turn_color tts__PlayerHandColor @The color of the player whose turn it currently is.
 Turns = {}
+
+---@return boolean
+function Turns.getNextTurnColor() end
+
+---@return boolean
+function Turns.getPreviousTurnColor() end
