35ce74b0cd9b0b482683dcea76016b0c71c723a3
Benjamin Dobell <git@benjamin.dobell.email>
2024-08-05 21:49:21 +1000

    Refined printToColor/broadcastToColor color to tts__PlayerColor.
    Additionally replaced overloads with optional parameters.

--- tts/global.def.lua
+++ tts/global.def.lua
@@ -1,33 +1,27 @@
-
----@overload fun(label: string, callback: fun(playerColor: tts__PlayerColor, object: tts__Object, pointerPosition: nil | tts__Vector, isKeyUp: boolean): void): true
 ---@param label string
 ---@param callback fun(playerColor: tts__PlayerColor, object: tts__Object, pointerPosition: nil | tts__Vector, isKeyUp: boolean): void
----@param triggerOnKeyUp boolean @Defaults to false
+---@param triggerOnKeyUp? boolean @Defaults to false
 ---@return true
 function addHotkey(label, callback, triggerOnKeyUp) end
 
----@overload fun(label: string, callback: (fun(playerColor: tts__PlayerHandColor, menuPosition: nil | tts__Vector): void)): true
----@overload fun(label: string, callback: (fun(playerColor: tts__PlayerHandColor, menuPosition: nil | tts__Vector): void), keepOpen: boolean): true
 ---@param label string @Text for the menu item.
 ---@param callback fun(playerColor: tts__PlayerHandColor, menuPosition: nil | tts__Vector): void
----@param keepOpen boolean @Whether the context menu should remain open after the item is selected. Defaults to false.
----@param requireTable boolean @Whether the menu item is only included if the table is being hovered over. Defaults to false.
+---@param keepOpen? boolean @Whether the context menu should remain open after the item is selected. Defaults to false.
+---@param requireTable? boolean @Whether the menu item is only included if the table is being hovered over. Defaults to false.
 ---@return true @Technically, returns false if your `callback` param is nil. However, Luanalysis won't allow you to make that mistake.
 function addContextMenuItem(label, callback, keepOpen, requireTable) end
 
 ---@return true
 function clearContextMenu() end
 
----@overload fun(message: string): boolean
 ---@param message string
----@param color string
+---@param color? string
 ---@return boolean
 function broadcastToAll(message, color) end
 
----@overload fun(message: string, playerColor: string): boolean
 ---@param message string
----@param playerColor string
----@param messageColor string
+---@param playerColor tts__PlayerColor
+---@param messageColor? string
 ---@return boolean
 function broadcastToColor(message, playerColor, messageColor) end
 
@@ -35,36 +29,28 @@ function broadcastToColor(message, playerColor, messageColor) end
 ---@return (tts__Container | tts__Stackable)[]
 function group(objects) end
 
----@overload fun(value: any): string
----@overload fun(value: any, label: string): string
 ---@param value any
----@param label string
----@param tags string
+---@param label? string
+---@param tags? string
 ---@return string
 function log(value, label, tags) end
 
----@overload fun(value: any): string
----@overload fun(value: any, label: string): string
----@overload fun(value: any, label: string, tag: string): string
----@overload fun(value: any, label: string, tag: string, concise: boolean): string
 ---@param value any
----@param label string
----@param tag string
----@param concise boolean @Default false
----@param displayTag boolean @Default false
+---@param label? string
+---@param tag? string
+---@param concise? boolean @Default false
+---@param displayTag? boolean @Default false
 ---@return string
 function logString(value, label, tag, concise, displayTag) end
 
----@overload fun(message: string): boolean
 ---@param message string
----@param color tts__ColorShape
+---@param color? tts__ColorShape
 ---@return boolean
 function printToAll(message, color) end
 
----@overload fun(message: string, playerColor: string): boolean
 ---@param message string
----@param playerColor string
----@param messageColor string
+---@param playerColor tts__PlayerColor
+---@param messageColor? string
 ---@return boolean
 function printToColor(message, playerColor, messageColor) end
 
